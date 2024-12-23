a47cea83769c2f25edd0081bb65fb9ee62f6738c
Lechros <0onionion0@gmail.com>
2024-12-23 13:55:08 +0900

    feat: add capturing group option and fix fuzzy combined choseong match

--- hangul_regexp.go
+++ hangul_regexp.go
@@ -6,7 +6,7 @@ import (
 	"unicode/utf8"
 )
 
-func GetPattern(search string, ignoreSpace bool, fuzzy bool, matchChoseong bool) (string, error) {
+func GetPattern(search string, ignoreSpace bool, fuzzy bool, matchChoseong bool, capturing bool) (string, error) {
 	if ignoreSpace && fuzzy {
 		return "", errors.New("ignoreSpace and fuzzy cannot be true at the same time")
 	}
@@ -20,28 +20,28 @@ func GetPattern(search string, ignoreSpace bool, fuzzy bool, matchChoseong bool)
 	}
 
 	builder := strings.Builder{}
-	builder.Grow(preCalculateBytes(search, len(connector), matchChoseong))
+	builder.Grow(preCalculateBytes(search, len(connector), matchChoseong, capturing))
 
 	for i, ch := range search {
 		if i+utf8.RuneLen(ch) == len(search) {
 			if IsHangul(ch) {
-				writeLastHangulPattern(&builder, ch, connector)
+				writeLastHangulPattern(&builder, ch, connector, capturing)
 			} else if CanBeChoseong(ch) {
-				writeChoseongPattern(&builder, ch)
+				writeChoseongPattern(&builder, ch, capturing)
 			} else if matchChoseong && CanBeChoseongOrJongseong(ch) {
-				writeCombinedChoseongPattern(&builder, ch)
+				writeCombinedChoseongPattern(&builder, ch, connector, capturing)
 			} else {
-				writeEscaped(&builder, ch)
+				writeEscaped(&builder, ch, capturing)
 			}
 		} else {
 			if matchChoseong && CanBeChoseongOrJongseong(ch) {
 				if CanBeChoseong(ch) {
-					writeChoseongPattern(&builder, ch)
+					writeChoseongPattern(&builder, ch, capturing)
 				} else {
-					writeCombinedChoseongPattern(&builder, ch)
+					writeCombinedChoseongPattern(&builder, ch, connector, capturing)
 				}
 			} else {
-				writeEscaped(&builder, ch)
+				writeEscaped(&builder, ch, capturing)
 			}
 			builder.WriteString(connector)
 		}
@@ -50,7 +50,7 @@ func GetPattern(search string, ignoreSpace bool, fuzzy bool, matchChoseong bool)
 	return builder.String(), nil
 }
 
-func preCalculateBytes(str string, connectorLength int, matchChoseong bool) int {
+func preCalculateBytes(str string, connectorLength int, matchChoseong bool, capturing bool) int {
 	if matchChoseong {
 		size := len(str)
 		for _, ch := range str {
@@ -60,69 +60,114 @@ func preCalculateBytes(str string, connectorLength int, matchChoseong bool) int 
 			}
 			// Accommodate for connector and possible regex character escape
 			size += connectorLength + 1
+			if capturing {
+				size += 2
+			}
 		}
 		// Last hangul pattern is max. 28 bytes
 		size += 28 - 3
+		if capturing {
+			size += 4
+		}
 		return size
+	} else if capturing {
+		return len(str) + utf8.RuneCountInString(str)*(connectorLength+1+2) + (28 - 3 + 4)
 	} else {
 		return len(str) + utf8.RuneCountInString(str)*(connectorLength+1) + (28 - 3)
 	}
 }
 
-func writeEscaped(builder *strings.Builder, ch rune) {
+func writeEscaped(builder *strings.Builder, ch rune, capturing bool) {
+	if capturing {
+		builder.WriteRune('(')
+	}
 	switch ch {
 	case '.', '^', '$', '*', '+', '?', '(', ')', '[', '{', '\\', '|':
 		builder.WriteRune('\\')
 	}
 	builder.WriteRune(ch)
+	if capturing {
+		builder.WriteRune(')')
+	}
 }
 
-func writeChoseongPattern(builder *strings.Builder, choseong rune) {
+func writeChoseongPattern(builder *strings.Builder, choseong rune, capturing bool) {
 	choOffset := GetChoseongOffset(choseong)
-	builder.WriteString("(?:")
-	builder.WriteRune(choseong)
-	builder.WriteString("|[")
-	builder.WriteRune(Assemble(choOffset, 0, 0))
-	builder.WriteRune('-')
-	builder.WriteRune(Assemble(choOffset, len(jungseongs)-1, len(jongseongs)-1))
-	builder.WriteString("])")
+	if capturing {
+		builder.WriteRune('(')
+		builder.WriteRune(choseong)
+		builder.WriteString("|[")
+		builder.WriteRune(Assemble(choOffset, 0, 0))
+		builder.WriteRune('-')
+		builder.WriteRune(Assemble(choOffset, len(jungseongs)-1, len(jongseongs)-1))
+		builder.WriteString("])")
+	} else {
+		builder.WriteString("(?:")
+		builder.WriteRune(choseong)
+		builder.WriteString("|[")
+		builder.WriteRune(Assemble(choOffset, 0, 0))
+		builder.WriteRune('-')
+		builder.WriteRune(Assemble(choOffset, len(jungseongs)-1, len(jongseongs)-1))
+		builder.WriteString("])")
+	}
 }
 
-func writeCombinedChoseongPattern(builder *strings.Builder, jongseong rune) {
+func writeCombinedChoseongPattern(builder *strings.Builder, jongseong rune, connector string, capturing bool) {
 	firstCho, secondCho := SplitJongseong(jongseong)
-	writeChoseongPattern(builder, firstCho)
-	writeChoseongPattern(builder, secondCho)
+	writeChoseongPattern(builder, firstCho, capturing)
+	builder.WriteString(connector)
+	writeChoseongPattern(builder, secondCho, capturing)
 }
 
-func writeLastHangulPattern(builder *strings.Builder, hangul rune, connector string) {
+func writeLastHangulPattern(builder *strings.Builder, hangul rune, connector string, capturing bool) {
 	choOffset, jungOffset, jongOffset := Disassemble(hangul)
 	if HasBatchim(hangul) {
 		jongseong := jongseongs[jongOffset]
 		if CanBeChoseong(jongseong) {
 			builder.WriteString("(?:")
-			builder.WriteRune(hangul)
+			writeRune(builder, hangul, capturing)
 			builder.WriteRune('|')
-			builder.WriteRune(Assemble(choOffset, jungOffset, 0))
+			writeRune(builder, Assemble(choOffset, jungOffset, 0), capturing)
 			builder.WriteString(connector)
-			writeChoseongPattern(builder, jongseong)
+			writeChoseongPattern(builder, jongseong, capturing)
 			builder.WriteRune(')')
 		} else {
 			firstJong, secondJong := SplitJongseong(jongseong)
 			builder.WriteString("(?:")
-			builder.WriteRune(hangul)
+			writeRune(builder, hangul, capturing)
 			builder.WriteRune('|')
-			builder.WriteRune(Assemble(choOffset, jungOffset, GetJongseongOffset(firstJong)))
+			writeRune(builder, Assemble(choOffset, jungOffset, GetJongseongOffset(firstJong)), capturing)
 			builder.WriteString(connector)
-			writeChoseongPattern(builder, secondJong)
+			writeChoseongPattern(builder, secondJong, capturing)
 			builder.WriteRune(')')
 		}
 	} else {
-		builder.WriteString("(?:")
-		builder.WriteRune(hangul)
-		builder.WriteString("|[")
-		builder.WriteRune(Assemble(choOffset, jungOffset, 1))
-		builder.WriteRune('-')
-		builder.WriteRune(Assemble(choOffset, jungOffset, len(jongseongs)-1))
-		builder.WriteString("])")
+		if capturing {
+			builder.WriteRune('(')
+			builder.WriteRune(hangul)
+			builder.WriteString("|[")
+			builder.WriteRune(Assemble(choOffset, jungOffset, 1))
+			builder.WriteRune('-')
+			builder.WriteRune(Assemble(choOffset, jungOffset, len(jongseongs)-1))
+			builder.WriteString("])")
+		} else {
+			builder.WriteString("(?:")
+			builder.WriteRune(hangul)
+			builder.WriteString("|[")
+			builder.WriteRune(Assemble(choOffset, jungOffset, 1))
+			builder.WriteRune('-')
+			builder.WriteRune(Assemble(choOffset, jungOffset, len(jongseongs)-1))
+			builder.WriteString("])")
+		}
+	}
+}
+
+func writeRune(builder *strings.Builder, ch rune, capturing bool) {
+	if capturing {
+		builder.WriteRune('(')
+		builder.WriteRune(ch)
+		builder.WriteRune(')')
+	} else {
+		builder.WriteRune(ch)
 	}
 }
--- hangul_regexp_test.go
+++ hangul_regexp_test.go
@@ -2,6 +2,7 @@ package hangul_regexp
 
 import (
 	"fmt"
+	"regexp"
 	"slices"
 	"strings"
 	"testing"
@@ -13,6 +14,7 @@ func TestGetPattern(t *testing.T) {
 		ignoreSpace bool
 		fuzzy       bool
 		choseong    bool
+		capturing   bool
 	}
 	tests := []struct {
 		name    string
@@ -20,35 +22,51 @@ func TestGetPattern(t *testing.T) {
 		want    string
 		wantErr bool
 	}{
-		{"Alphabet", args{"a", false, false, false}, "a", false},
-		{"Hangul", args{"ㄱ나다라123", false, false, false}, "ㄱ나다라123", false},
-		{"Mixed", args{"Zx0ㅡㅡ", false, false, false}, "Zx0ㅡㅡ", false},
-		{"Should escape", args{"[^가-힣]$", false, false, false}, "\\[\\^가-힣]\\$", false},
+		{"Alphabet", args{"a", false, false, false, false}, "a", false},
+		{"Hangul", args{"ㄱ나다라123", false, false, false, false}, "ㄱ나다라123", false},
+		{"Mixed", args{"Zx0ㅡㅡ", false, false, false, false}, "Zx0ㅡㅡ", false},
+		{"Should escape", args{"[^가-힣]$", false, false, false, false}, "\\[\\^가-힣]\\$", false},
 
-		{"Last char is choseong", args{"ㄱ", false, false, false}, "(?:ㄱ|[가-깋])", false},
-		{"Last char without batchim", args{"가 나", false, false, false}, "가 (?:나|[낙-낳])", false},
-		{"Last char with batchim", args{"가 안", false, false, false}, "가 (?:안|아(?:ㄴ|[나-닣]))", false},
-		{"Last char with double batchim", args{"가 있", false, false, false}, "가 (?:있|이(?:ㅆ|[싸-앃]))", false},
-		{"Last char with combined batchim", args{"가 얇", false, false, false}, "가 (?:얇|얄(?:ㅂ|[바-빟]))", false},
-		{"Last char is combined choseong", args{"ㄻ", false, false, false}, "ㄻ", false},
+		{"Last char is choseong", args{"ㄱ", false, false, false, false}, "(?:ㄱ|[가-깋])", false},
+		{"Last char without batchim", args{"가 나", false, false, false, false}, "가 (?:나|[낙-낳])", false},
+		{"Last char with batchim", args{"가 안", false, false, false, false}, "가 (?:안|아(?:ㄴ|[나-닣]))", false},
+		{"Last char with double batchim", args{"가 있", false, false, false, false}, "가 (?:있|이(?:ㅆ|[싸-앃]))", false},
+		{"Last char with combined batchim", args{"가 얇", false, false, false, false}, "가 (?:얇|얄(?:ㅂ|[바-빟]))", false},
+		{"Last char is combined choseong", args{"ㄻ", false, false, false, false}, "ㄻ", false},
 
-		{"Mixed / ignoreSpace=true", args{"ㅁ가a항1", true, false, false}, "ㅁ *?가 *?a *?항 *?1", false},
-		{"Last char with batchim / ignoreSpace=true / has space matcher between", args{"가 안", true, false, false}, "가 *?  *?(?:안|아 *?(?:ㄴ|[나-닣]))", false},
+		{"Mixed / ignoreSpace=true", args{"ㅁ가a항1", true, false, false, false}, "ㅁ *?가 *?a *?항 *?1", false},
+		{"Last char with batchim / ignoreSpace=true / has space matcher between", args{"가 안", true, false, false, false}, "가 *?  *?(?:안|아 *?(?:ㄴ|[나-닣]))", false},
 
-		{"Mixed / fuzzy=true", args{"ㅁ가a항1", false, true, false}, "ㅁ.*?가.*?a.*?항.*?1", false},
-		{"Space / fuzzy=true / spaces are not concatenated", args{"가 s", false, true, false}, "가.*? .*?s", false},
-		{"Last char with batchim / fuzzy=true / has any matcher between", args{"가 안", false, true, false}, "가.*? .*?(?:안|아.*?(?:ㄴ|[나-닣]))", false},
+		{"Mixed / fuzzy=true", args{"ㅁ가a항1", false, true, false, false}, "ㅁ.*?가.*?a.*?항.*?1", false},
+		{"Space / fuzzy=true / spaces are not concatenated", args{"가 s", false, true, false, false}, "가.*? .*?s", false},
+		{"Last char with batchim / fuzzy=true / has any matcher between", args{"가 안", false, true, false, false}, "가.*? .*?(?:안|아.*?(?:ㄴ|[나-닣]))", false},
 
-		{"Non-last char is choseong / choseong=true", args{"ㄱ1", false, false, true}, "(?:ㄱ|[가-깋])1", false},
-		{"Multiple choseong chars / choseong=true", args{"ㄱ ㄴㄷ", false, false, true}, "(?:ㄱ|[가-깋]) (?:ㄴ|[나-닣])(?:ㄷ|[다-딯])", false},
-		{"Mixed with choseong / choseong=true", args{"aㅎ1가ㄴ", false, false, true}, "a(?:ㅎ|[하-힣])1가(?:ㄴ|[나-닣])", false},
-		{"Standalone batchim char / choseong=true", args{"ㄻㅄ", false, false, true}, "(?:ㄹ|[라-맇])(?:ㅁ|[마-밓])(?:ㅂ|[바-빟])(?:ㅅ|[사-싷])", false},
+		{"Non-last char is choseong / choseong=true", args{"ㄱ1", false, false, true, false}, "(?:ㄱ|[가-깋])1", false},
+		{"Multiple choseong chars / choseong=true", args{"ㄱ ㄴㄷ", false, false, true, false}, "(?:ㄱ|[가-깋]) (?:ㄴ|[나-닣])(?:ㄷ|[다-딯])", false},
+		{"Mixed with choseong / choseong=true", args{"aㅎ1가ㄴ", false, false, true, false}, "a(?:ㅎ|[하-힣])1가(?:ㄴ|[나-닣])", false},
+		{"Standalone batchim char / choseong=true", args{"ㄻㅄ", false, false, true, false}, "(?:ㄹ|[라-맇])(?:ㅁ|[마-밓])(?:ㅂ|[바-빟])(?:ㅅ|[사-싷])", false},
 
-		{"Any / ignoreSpace=true, fuzzy=true / err", args{"", true, true, false}, "", true},
+		{"Any / ignoreSpace=true, fuzzy=true / err", args{"", true, true, false, false}, "", true},
+
+		{"Alphabet / capturing=true", args{"a", false, false, false, true}, "(a)", false},
+		{"Hangul / capturing=true", args{"ㄱ나다라123", false, false, false, true}, "(ㄱ)(나)(다)(라)(1)(2)(3)", false},
+		{"Mixed / capturing=true", args{"Zx0ㅡㅡ", false, false, false, true}, "(Z)(x)(0)(ㅡ)(ㅡ)", false},
+		{"Special chars / capturing=true", args{"[^가-힣]$", false, false, false, true}, "(\\[)(\\^)(가)(-)(힣)(])(\\$)", false},
+
+		{"Last char with batchim / capturing=true", args{"가 안", false, false, false, true}, "(가)( )(?:(안)|(아)(ㄴ|[나-닣]))", false},
+		{"Last char with double batchim / capturing=true", args{"가 있", false, false, false, true}, "(가)( )(?:(있)|(이)(ㅆ|[싸-앃]))", false},
+		{"Last char with combined batchim / capturing=true", args{"가 얇", false, false, false, true}, "(가)( )(?:(얇)|(얄)(ㅂ|[바-빟]))", false},
+		{"Last char is combined choseong / capturing=true", args{"ㄻ", false, false, false, true}, "(ㄻ)", false},
+
+		{"Mixed / fuzzy=true, capturing=true", args{"ㅁ가a항1", false, true, false, true}, "(ㅁ).*?(가).*?(a).*?(항).*?(1)", false},
+		{"Space / fuzzy=true, capturing=true", args{"가 s", false, true, false, true}, "(가).*?( ).*?(s)", false},
+		{"Last char with batchim / fuzzy=true, capturing=true", args{"가 안", false, true, false, true}, "(가).*?( ).*?(?:(안)|(아).*?(ㄴ|[나-닣]))", false},
+
+		{"Standalone batchim char / choseong=true, capturing=true", args{"ㄻㅄ", false, false, true, true}, "(ㄹ|[라-맇])(ㅁ|[마-밓])(ㅂ|[바-빟])(ㅅ|[사-싷])", false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := GetPattern(tt.args.search, tt.args.ignoreSpace, tt.args.fuzzy, tt.args.choseong)
+			got, err := GetPattern(tt.args.search, tt.args.ignoreSpace, tt.args.fuzzy, tt.args.choseong, tt.args.capturing)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("GetPattern() error = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -60,39 +78,69 @@ func TestGetPattern(t *testing.T) {
 	}
 }
 
+func TestGetPatternCapturing(t *testing.T) {
+	tests := []struct {
+		search      string
+		target      string
+		matchConcat string
+	}{
+		{"이이저", "이글아이 레인저슈트", "이이저"},
+		{"마깃아", "마력이 깃든 안대", "마깃안"},
+		{"루컨ㅁ", "루즈 컨트롤 머신 마크", "루컨머"},
+		{"ㅇㅋㅇ", "아케인셰이드 스태프", "아케인"},
+		{"낢", "날아 먹", "날먹"},
+		{"ㅄ", "보라색", "보색"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.search+" / "+tt.target, func(t *testing.T) {
+			pattern, _ := GetPattern(tt.search, false, true, true, true)
+			println(pattern)
+			regex := regexp.MustCompile(pattern)
+			match := regex.FindStringSubmatch(tt.target)
+			for _, m := range match {
+				println(m)
+			}
+			actual := strings.Join(match[1:], "")
+			if actual != tt.matchConcat {
+				t.Errorf("GetPattern() got = %v, want %v", actual, tt.matchConcat)
+			}
+		})
+	}
+}
+
 func BenchmarkGetPattern_마깃안(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = GetPattern("마깃안", false, false, false)
+		_, _ = GetPattern("마깃안", false, false, false, false)
 	}
 }
 
 func BenchmarkGetPattern_마깃안_fuzzy(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = GetPattern("마깃안", false, true, false)
+		_, _ = GetPattern("마깃안", false, true, false, false)
 	}
 }
 
 func BenchmarkGetPattern_아케인셰이드_에너지소드(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = GetPattern("아케인셰이드 에너지소드", false, false, false)
+		_, _ = GetPattern("아케인셰이드 에너지소드", false, false, false, false)
 	}
 }
 
 func BenchmarkGetPattern_아케인셰이드_에너지소드_fuzzy(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = GetPattern("아케인셰이드 에너지소드", false, true, false)
+		_, _ = GetPattern("아케인셰이드 에너지소드", false, true, false, false)
 	}
 }
 
 func BenchmarkGetPattern_아케인셰이드_에너지소드_fuzzy_matchChoseong(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = GetPattern("아케인셰이드 에너지소드", false, true, true)
+		_, _ = GetPattern("아케인셰이드 에너지소드", false, true, true, false)
 	}
 }
 
 func BenchmarkGetPattern_ㅇㅋㅇㅅㅇㄷ_ㅇㄴㅈㅅㄷ_fuzzy_matchChoseong(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		_, _ = GetPattern("ㅇㅋㅇㅅㅇㄷ ㅇㄴㅈㅅㄷ", false, true, true)
+		_, _ = GetPattern("ㅇㅋㅇㅅㅇㄷ ㅇㄴㅈㅅㄷ", false, true, true, false)
 	}
 }
 
